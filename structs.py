2e48c82a75f0803eb7408902baf03142bafab444
alinka897 <alina.agapova9@yandex.ru>
2025-10-13 20:57:35 +0300

    +tr и штраф за shift

--- structs.py
+++ structs.py
@@ -1,18 +1,20 @@
-larm = []
-for i in range(16, 21):
+larm = [1]
+for i in range(2, 21):
     larm += [i, i + 14, i + 28]
-    i += 1
 
 
 class Key():
-    def __init__(self, code: int, char: str, /):
-        if not (code in range(16, 54)):
+    def __init__(self, code: int, char: str, /, alt=False):
+        if not (code in range(1, 54)):
             print("Неправильный номер клавиши")
         self.code = code
         self.char = char
+        self.alt = alt
+# 1 - ~ 29 - \
 # row
-
-        if code in range(16, 28):
+        if code in range(1, 14):
+            self.row = "tr"
+        elif code in range(16, 29):
             self.row = "ur"
         elif code in range(30, 41):
             self.row = "hr"
@@ -25,39 +27,43 @@ class Key():
         else:
             self.arm = "r"
 # finger
-        if code in [16, 30, 44, 25, 26, 27, 39, 40, 53]:
+        if code in [1, 2, 16, 11, 12, 13, 30, 44, 25, 26, 27, 28, 39, 40, 53]:
             self.finger = 'f5'
-        elif code in [17, 31, 45, 24, 38, 52]:
+        elif code in [3, 10, 17, 31, 45, 24, 38, 52]:
             self.finger = 'f4'
-        elif code in [18, 32, 46, 23, 37, 51]:
+        elif code in [4, 9, 18, 32, 46, 23, 37, 51]:
             self.finger = 'f3'
         else:
             self.finger = 'f2'
 # penalty
         if code in list(range(30, 34)) + list(range(36, 40)):
             self.penalty = 0
-        elif code in [20, 21, 48, 49, 26]:
+        elif code in (list(range(2, 6)) + [20, 21, 48, 49, 26] +
+                      list(range(8, 12))):
             self.penalty = 2
-        elif code in [27, 41]:
+        elif code in [27, 1, 6, 7, 12]:
             self.penalty = 3
+        elif code in [13, 29]:
+            self.penalty = 4
         else:
             self.penalty = 1
 
 
 class Layout():
-    def __init__(self, ur='йцукенгшщзхъ', hr='фывапролджэ', lr='ячсмитьбю.',
-                 /, alt=False):
-        if not ([len(r) for r in (ur, hr, lr)] == [12, 11, 10]):
+    def __init__(self, tr='ё1234567890-=', ur='йцукенгшщзхъ\\', hr='фывапролджэ',
+                 lr='ячсмитьбю.'):
+        if not ([len(r) for r in (tr, ur, hr, lr)] == [13, 13, 11, 10]):
             print("Ряды введены неправильно")
             return
+        self.tr = tr
         self.ur = ur
         self.hr = hr
         self.lr = lr
-        self.alt = alt
         self.extract_keys()
 
     def extract_keys(self):
-        d = dict(list(zip(self.ur, range(16, 28))) +
+        d = dict(list(zip(self.tr, range(1, 14))) +
+                 list(zip(self.ur, range(16, 29))) +
                  list(zip(self.hr, range(30, 41))) +
                  list(zip(self.lr, range(44, 54))))
         keys = dict()
@@ -71,7 +77,7 @@ class Layout():
             text = f.readlines()
             for line in text:
                 pen_counter += self.pen_count(line)
-        print(f'Кол-во штрафов: {pen_counter}')
+        print(f'Кол-во штрафов в файле {filename}: {pen_counter}')
 
     def lexeme(self, filename, /):
         with open(filename) as f:
@@ -80,13 +86,21 @@ class Layout():
             for line in text:
                 penalty = self.pen_count(line)
                 f.write(line[:-1] + ' ' + str(penalty) + '\n')
+        print("Штрафы записаны в файл result.txt")
 
     def pen_count(self, line: str, /):
         pen_counter = 0
         for ch in line:
-            k = self.keys.get(ch, 0)
+            if not ch.isalpha():
+                continue
+            k = self.keys.get(ch.lower(), 0)
             if k == 0:
-                pass
+                continue
             else:
+                if ch.isupper():
+                    pen_counter += 2  # shift
+                if k.alt:
+                    pen_counter += 1  # alt
                 pen_counter += k.penalty
+            print(pen_counter)
         return pen_counter
